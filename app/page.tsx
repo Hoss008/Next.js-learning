bc89b492d57f96417c8b88441f39e95d6457e998
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2026-02-22 17:06:08 +0200

    styling added

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      Welcome to NEXT JS 16
-    </div>
+    <section>
+      <h1 className='text-center'> The HUb for Every Dev <br/> You Can't Miss</h1>
+      <p className='text-center mt-5'>Hackthons, Meetups and Conferneces, All in One Place</p>
+    </section>
   )
 }
