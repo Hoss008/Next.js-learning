2f24929236b4a9b874778c3e5641b2158f5b5b44
Hoss008 <43291152+Hoss008@users.noreply.github.com>
2026-02-23 03:18:28 +0200

    home page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import EventCard from '@/components/EventCard'
 import ExploreBtn from '@/components/ExploreBtn'
 import React from 'react'
+import { events } from '@/lib/constants'
+
+// const events = [
+//   {image:'/images/event1.png', title:"Event1" , slug:"event-1" , location:'location-1' , date:'Date-1',time:'Time-1'},
+//   {image:'/images/event2.png', title:"Event2"}
+// ]
 
 export default function page() {
   return (
@@ -10,14 +17,16 @@ export default function page() {
 
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
-        <ul className="events">
-          {[1, 2, 3, 4, 5].map((event) => (
-            <li key={event}>Event {event}</li>
+        <ul className="events list-none">
+          {events.map((event) => (
+            <li key={event.title}>
+              <EventCard {...event}/>
+            </li>
           ))}
         </ul>
       </div>
 
-      
+
     </section>
   )
 }
